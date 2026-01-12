c6373a81b7a70d447eebde725ba590533367373b
hvu05 <hoangvu06052005@gmail.com>
2026-01-12 09:15:08 +0700

    add test

--- src/routes/telegram.route.ts
+++ src/routes/telegram.route.ts
@@ -4,5 +4,6 @@ import { telegramWebhook } from "../controllers/telegram.controller.js";
 const routerTelegram = Router()
 
 routerTelegram.post('/webhook', telegramWebhook)
+routerTelegram.get('/test', (req, res) => {res.send('Hello World')})
 
 export default routerTelegram
